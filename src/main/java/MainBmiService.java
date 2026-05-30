18bea96b9aebe58e74bebfe752cc6cce9c0b2e3f
Aleksandra Nikitina <modistka714@gmail.com>
2026-05-30 17:11:51 +0300

    adjust main classes accordingly

---
+++ src/main/java/MainBmiService.java
@@ -0,0 +1,15 @@
+public class MainBmiService {
+
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        double rost = 1.87;
+        int ves = 98;
+
+        int index = service.calculate(rost, ves);
+
+        System.out.println(index);
+
+    }
+
+}
+
---
+++ src/main/java/MainBonusMilesService.java
@@ -0,0 +1,9 @@
+public class MainBonusMilesService {
+    public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+        int price = 10000;
+        int miles = service.calculate(price); // должно получиться 500
+        System.out.println(miles);
+    }
+}
+
